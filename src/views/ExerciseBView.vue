3d75d4cb1d245d5c17cd4a7dabcd12330a3d744d
Christian Hegedüs <git@christianhegedues.de>
2022-05-25 20:34:22 +0200

    add exercise b

--- src/views/ExerciseBView.vue
+++ src/views/ExerciseBView.vue
@@ -1,6 +1,35 @@
+<script setup>
+import Counter from "@/components/Counter.vue";
+import supabase from "../supabase.js";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const subscription = ref(null)
+const count = ref(0)
+
+async function updateCount(increase) {
+  await supabase.from('counter').upsert({ 
+    id: 3,
+    count: count.value + increase
+  })
+}
+
+onMounted(async () => {
+  const {data} = await supabase.from('counter').select('count').eq('id', 3).single()
+  count.value = data.count
+
+  subscription.value = supabase.from('counter').on('UPDATE', (payload) => {
+    count.value = payload.new.count
+  }).subscribe()
+})
+
+onBeforeUnmount(() => {
+  supabase.removeSubscription(subscription.value)
+})
+</script>
+
 <template>
   <section class="exercise-b">
-    <p>Aufgabe 2</p>
+    <Counter :count="count" @update:count="updateCount" />
   </section>
 </template>
 
